dff5f87b73f0a6ce9951eea2d3eb90d1f1e56f08
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-12-12 16:35:25 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -7,7 +7,7 @@ bool ricerca(char lista [10][20], char nome[1][20], int &posizione)
   {
     if(strcmp (lista[i], nome[0]) == 0)
     {
-      pos = i;
+      posizione = i;
       
       return true;
     }
