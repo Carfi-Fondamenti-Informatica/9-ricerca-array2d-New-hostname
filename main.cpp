0003ea2a489ee8b51dff3a1aaa39ad1ea1885500
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-12-12 16:25:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  
+  int posizione;
+  char a[10][20], f[1][20]
+    
+  for(int i = 0; i < 10; i++)
+  {
+    cin >> a[i];
+  }
+  
+  cin >> f[0];
+  
+  if(ricerca(a, f, posizione))
+  {
+    cout << posizione << endl;
+  }
+  else
+  {
+    cout << "Non presente" << endl;
+  }
+  
   return 0;
 }
